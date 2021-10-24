f1ba4d5c0ec722805f57f095b700f03379f6af57
Monijo <mokajozefowicz@gmail.com>
2021-10-24 14:06:27 +0200

    game_html

--- game.py
+++ game.py
@@ -20,6 +20,27 @@ Start_HTML = """
 </html>
 """
 
+Game_HTML = """
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Guess the number</title>
+</head>
+<body>
+<h1> It is number {guess}</h1>
+<form action="" method ="POST">
+    <input type="submit" name="user_answer" value="too big">
+    <input type="submit" name="user_answer" value="too small">
+    <input type="submit" name="user_answer" value="You won!">
+    <input type="hidden" name="min value= "{min}">
+    <input type="hidden" name="min value= "{max}">
+    <input type = "hidden" name="guess" value="{guess}"
+</form>
+</body>
+</html>
+"""
+
 
 
 
